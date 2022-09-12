35cd79d8c78162724c71e9dc969720150ffc6ec2
Emorinken <emorinkensam@gmail.com>
2022-09-12 12:40:20 -0700

    fourth commit

--- src/components/MobileNav.jsx
+++ src/components/MobileNav.jsx
@@ -7,7 +7,7 @@ const MobileNav = ({HideMobileNav}) => {
 		AOS.init({duration: 2000})
 	})
 	return ( 
-		<div className=" flex  flex-col justify-center  md:hidden items-center w-screen absolute bg-red-400 z-50 left-0 top-0 h-screen" data-aos="fade-left">
+		<div className=" flex  flex-col justify-center bg-black md:hidden items-center w-screen absolute  z-50 left-0 top-0 h-screen" data-aos="fade-left">
 			<svg onClick={HideMobileNav} style={{
 				fill:"#fff"
 			}} className="absolute top-[5%] right-[2%]" width="40px" height="40px" viewBox="0 0 200 200" data-name="Layer 1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"><title/><path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z"/></svg>
